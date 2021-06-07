390abf24bfffa324a58685dce075d36fe78dbfb7
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-06-07 16:52:56 -0600

    Make tech article fetch call

---
+++ src/apiCalls.js
@@ -0,0 +1,11 @@
+const NYT_API_KEY = process.env.REACT_APP_NYT_KEY
+
+export const getTechArticles = () => {
+  return fetch(`https://api.nytimes.com/svc/topstories/v2/technology.json?api-key=${NYT_API_KEY}`)
+  .then((response) => {
+    if(!response.ok) {
+      throw Error('Unable to get these articles at the moment, please refresh and/or try again later.')
+    }
+    return response.json()
+  })
+}
