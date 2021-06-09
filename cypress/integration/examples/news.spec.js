4a43a2946f48c17bac1f75f0e7b1d570e5d56179
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-06-09 11:46:58 -0600

    Finish cards test

--- cypress/integration/examples/news.spec.js
+++ cypress/integration/examples/news.spec.js
@@ -7,12 +7,30 @@ context('Actions', () => {
     cy.visit('http://localhost:3000/')
   })
 
-  // https://on.cypress.io/interacting-with-elements
+
 
   it('Should have a header', () => {
     cy
     .get('[data-cy=header]').should('exist')
     .should('contain', 'News Of THe World')
+  })
+
+  it.only('Should have articles for the user to choose from on a series of cards', () => {
+    cy
+    .get('[data-cy=card_title]').should('exist')
+    .should('contain', 'Uber and Lyft Ramp Up Legislative Efforts to Shield Business Model')
+    .get('[data-cy=card_photo]').should('exist')
+    .should('have.attr', 'src', 'https://static01.nyt.com/images/2021/06/07/nyregion/00gig-worker01/00gig-worker01-articleInline.jpg')
+    .get('[data-cy=card_caption]').should('exist')
+    .should('contain', 'Driving for Uber in Manhattan. Gig companies like Uber and Lyft have long resisted classifying workers as employees.')
+    .get('[data-cy=card_authors]').should('exist')
+    .should('contain', 'By Noam Scheiber')
+    .get('[data-cy=card_authors]').should('exist')
+    .should('contain', 'By Noam Scheiber')
+    .get('[data-cy=card_button]').should('exist')
+    .should('contain', 'See More')
+    .get('[data-cy=card_date]').should('exist')
+    .should('contain', 'Wed Jun 09 2021')
   })
 
 })
--- src/Card/Card.js
+++ src/Card/Card.js
@@ -11,9 +11,9 @@ function Card({ title, img, caption, authors, updated }) {
       <h3 className='card_title' data-cy='card_title'>{title}</h3>
       <img className='card_photo' data-cy='card_photo'src={img}/>
       <p className='card_caption' data-cy='card_caption'>{caption}</p>
-      <p>{authors}</p>
+      <p className='card_authors' data-cy='card_authors'>{authors}</p>
       <div className='card_button-date-container'>
-        <Link to={`/article/${title}`}>See More</Link>
+        <Link to={`/article/${title}`} className='card_button' data-cy='card_button'>See More</Link>
         <p className='card_date' data-cy='card_date'>{readableDate}</p>
       </div>
     </div>
