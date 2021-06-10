fe3ce1812cf1b8931c07b3d8fa1a3f7feb9b42c4
Max Bregman <67295227+Max9545@users.noreply.github.com>
2021-06-09 18:16:41 -0600

    Add card styles

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -10,7 +10,7 @@ function Card({ title, img, caption, authors, updated }) {
     <div className='card' data-cy='card'>
       <h3 className='card_title' data-cy='card_title'>{title}</h3>
       <img className='card_photo' data-cy='card_photo'src={img}/>
-      <p className='card_caption' data-cy='card_caption'>{caption}</p>
+      {/* <p className='card_caption' data-cy='card_caption'>{caption}</p> */}
       <p className='card_authors' data-cy='card_authors'>{authors}</p>
       <div className='card_button-date-container'>
         <Link to={`/article/${title}`} className='card_button' data-cy='card_button'>See More</Link>
